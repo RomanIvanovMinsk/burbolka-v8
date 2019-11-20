2d5dcc4fea94962f42715ed984a80ccf302e58f9
burbolka <badbird87@gmail.com>
2019-11-20 17:53:22 +0300

    install models builder and switch to use models from Burbolka.Models project in example views

--- Sources/Burbolka.Models/ContentModels/Builder.cs
+++ Sources/Burbolka.Models/ContentModels/Builder.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Burbolka.Models.ContentModels
+﻿namespace Burbolka.Models.ContentModels
 {
-    class Builder
+    internal class Builder
     {
     }
 }
